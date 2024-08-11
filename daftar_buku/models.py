526e6aa5435bff89e7a190b390ef17768d55d361
RobertBenyamin <robertbenyamino@gmail.com>
2024-08-11 21:55:19 +0700

    feat: update models.py to use CharField without max_length

--- daftar_buku/models.py
+++ daftar_buku/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 #make model books in accordance with books.csv
 class Buku(models.Model):
-    isbn = models.CharField(max_length=20)
-    judul = models.CharField(max_length=100)
-    penulis = models.CharField(max_length=100)
+    isbn = models.CharField()
+    judul = models.CharField()
+    penulis = models.CharField()
     tahun = models.IntegerField()
-    kategori = models.CharField(max_length=100)
-    gambar = models.CharField(max_length=100)
+    kategori = models.CharField()
+    gambar = models.CharField()
     deskripsi = models.TextField()
     rating = models.ForeignKey('Rating', on_delete=models.CASCADE, default=0)
     def __str__(self):
